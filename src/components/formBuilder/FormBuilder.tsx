f879934cd516280c2118af2a4ab94b8399f8300f
Damon <126731021+damon314159@users.noreply.github.com>
2024-01-16 18:59:44 +0000

    docs: comment form builder

--- src/components/formBuilder/FormBuilder.tsx
+++ src/components/formBuilder/FormBuilder.tsx
@@ -6,13 +6,16 @@ import {
   type FormBuilderField
 } from '../../interfaces/FormBuilderProps'
 
+// Take a collection of field objects to create a React controlled form
 function FormBuilder({ formFields }: FormBuilderProps): JSX.Element {
+  // Create state that tracks value of each input field
   const [formValues, setFormValues] = useState(
     formFields.reduce(
       (
         initialState: Record<string, string>,
         field: FormBuilderField
       ): Record<string, string> => {
+        // Take each field in turn, and write {name: ''} to initial state object
         const newField: Record<string, string> = {}
         newField[field.name] = ''
         return Object.assign(initialState, newField)
@@ -24,9 +27,11 @@ function FormBuilder({ formFields }: FormBuilderProps): JSX.Element {
     <Form
       fields={formFields.map(
         (formField): FormFieldProps => ({
+          // Fill in the input field props algorithmically
           name: formField.name,
           type: formField.type,
           value: formValues[formField.name],
+          // Write to state via a closured onChange function
           onChange: (event: ChangeEvent<HTMLInputElement>) => {
             const newValues = { ...formValues }
             newValues[formField.name] = event.target.value
