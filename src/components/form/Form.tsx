fb318e9e44b825858774c682db43675d9dffeb98
Damon <126731021+damon314159@users.noreply.github.com>
2024-01-18 16:06:20 +0000

    feat: simple form stylings

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,6 +1,7 @@
 import { type FormEvent } from 'react'
 import type FormFieldProps from '../../interfaces/FormFieldProps'
 import './Form.css'
+import titleCase from '../../helpers/titleCase'
 
 interface FormProps {
   fields: FormFieldProps[]
@@ -13,16 +14,22 @@ function Form({ fields, onSubmit }: FormProps): JSX.Element {
     <form autoComplete="true" onSubmit={onSubmit}>
       {fields.map((formField: FormFieldProps) => (
         // Map each field into an input element with corresponding attributes
-        <input
-          key={formField.id}
-          name={formField.name}
-          type={formField.type}
-          value={formField.value}
-          onChange={formField.onChange}
-          required={formField.required}
-        />
+        <div className="input-wrapper" key={formField.id}>
+          <label htmlFor={formField.name}>{titleCase(formField.name)}</label>
+          <input
+            id={formField.name}
+            name={formField.name}
+            type={formField.type}
+            value={formField.value}
+            onChange={formField.onChange}
+            required={formField.required}
+          />
+        </div>
       ))}
-      <button type="submit">Submit</button>
+      <div className="btn-wrapper">
+        <button type="submit">Submit</button>
+        <button type="button">Cancel</button>
+      </div>
     </form>
   )
 }
