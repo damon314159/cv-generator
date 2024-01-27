445648123e2a00c4da371fff5b392b9c3edfd975
Damon <126731021+damon314159@users.noreply.github.com>
2024-01-27 15:59:09 +0000

    feat: rearrange App component for CvView

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Info from './components/info/Info'
 import Education from './components/education/Education'
 import Experience from './components/experience/Experience'
+import CvView from './components/cvView/CvView'
 
 function App(): JSX.Element {
   const [userInfo, setUserInfo] = useState<Record<string, string> | null>(null)
@@ -10,19 +11,32 @@ function App(): JSX.Element {
   const [experiences, setExperiences] = useState<Record<string, string>[]>([])
 
   return (
-    <div className="editors">
-      <section className="info">
-        <Info userInfo={userInfo} setUserInfo={setUserInfo} />
-      </section>
+    <>
+      <div className="editors">
+        <section className="info">
+          <Info userInfo={userInfo} setUserInfo={setUserInfo} />
+        </section>
 
-      <section className="education">
-        <Education educations={educations} setEducations={setEducations} />
-      </section>
+        <section className="education">
+          <Education educations={educations} setEducations={setEducations} />
+        </section>
 
-      <section className="experience">
-        <Experience experiences={experiences} setExperiences={setExperiences} />
-      </section>
-    </div>
+        <section className="experience">
+          <Experience
+            experiences={experiences}
+            setExperiences={setExperiences}
+          />
+        </section>
+      </div>
+
+      <div className="cv-view">
+        <CvView
+          userInfo={userInfo}
+          educations={educations}
+          experiences={experiences}
+        />
+      </div>
+    </>
   )
 }
 
